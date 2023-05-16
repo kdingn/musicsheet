067f2b28859724579338efd92af1cb1645044042
kdingn <koukou3335@gmail.com>
2023-05-16 19:22:44 +0900

    体裁の調整

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-toolbar color="primary">
+    <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon>
+    <v-toolbar-title class="white--text">{{ title }}</v-toolbar-title>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    title: "Title",
+  }),
+};
+</script>
---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-text-field v-model="message" label="v-text-field"></v-text-field>
+    <v-card color="primary">
+      <v-card-subtitle>
+        <span class="white--text">v-card</span>
+      </v-card-subtitle>
+      <v-card-text>
+        <span class="white--text">{{ message }}</span>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    message: "メッセージです",
+  }),
+};
+</script>
