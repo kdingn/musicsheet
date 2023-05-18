ae643289af310e6c64b95ecac342ffcfd5edf91b
kdingn <koukou3335@gmail.com>
2023-05-19 00:44:20 +0900

    chordjs の追加

---
+++ src/assets/chord.js
@@ -0,0 +1,65 @@
+const roots = [
+  { C: 1 },
+  { "C#": 2 },
+  { Db: 2 },
+  { D: 3 },
+  { "D#": 4 },
+  { Eb: 4 },
+  { E: 5 },
+  { F: 6 },
+  { "F#": 7 },
+  { Gb: 7 },
+  { G: 8 },
+  { "G#": 9 },
+  { Ab: 9 },
+  { A: 10 },
+  { "A#": 11 },
+  { Bb: 11 },
+  { B: 0 },
+];
+const ctypes = [
+  { "": [1, 5, 8] },
+  { m: [1, 4, 8] },
+  { dim: [1, 4, 7] },
+  { aug: [1, 5, 9] },
+  { sus4: [1, 4, 8] },
+  { 7: [1, 5, 8, 11] },
+  { m7: [1, 4, 8, 11] },
+  { "△7": [1, 5, 8, 12] },
+  { 69: [1, 5, 10, 15] },
+  { m69: [1, 4, 10, 15] },
+  { φ: [1, 4, 7, 11] },
+];
+
+let chordAll = [];
+for (let root of roots) {
+  for (let ctype of ctypes) {
+    let notes = [];
+    for (let note of ctype[Object.keys(ctype)]) {
+      notes.push((root[Object.keys(root)] + note - 1) % 12);
+    }
+    chordAll.push({
+      root: String(Object.keys(root)),
+      ctype: String(Object.keys(ctype)),
+      notes: notes,
+    });
+  }
+}
+
+const rootAll = {
+  C: 1,
+  "C#": 2,
+  D: 3,
+  Eb: 4,
+  E: 5,
+  F: 6,
+  "F#": 7,
+  G: 8,
+  Bb: 11,
+  B: 0,
+};
+
+export default {
+  rootAll,
+  chordAll,
+};
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,19 +1,134 @@
 <template>
   <v-container>
-    <v-contrainer>
-      <v-spacer></v-spacer>
-      aaa
-      <v-row no-gutters>
-        <v-col v-for="n in 3" :key="n">
-          <v-card class="pa-2" outlined tile> なかみ </v-card>
-        </v-col>
-      </v-row>
-    </v-contrainer>
+    <v-row>
+      <v-col cols="8">
+        <v-text-field
+          label="title"
+          outlined
+          dense
+          v-model="title"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="4">
+        <v-select
+          label="key"
+          outlined
+          dense
+          v-model="key"
+          :items="Object.keys(rootAll)"
+        ></v-select>
+      </v-col>
+    </v-row>
+
+    <div class="mb-2">
+      01.
+      <v-contrainer>
+        <v-row no-gutters>
+          <v-col>
+            <v-card class="pa-2" outlined tile>
+              <span class="text-body-1">F</span>
+              <span class="text-body-1">7</span>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card class="pa-2" outlined tile>
+              <span class="text-body-1">Bb</span>
+              <span class="text-body-1">7</span>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card class="pa-2" outlined tile>
+              <span class="text-body-1">F</span>
+              <span class="text-body-1">7</span>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card class="pa-2" outlined tile>
+              <span class="text-body-1">F</span>
+              <span class="text-body-1">7</span>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-contrainer>
+    </div>
+    <div class="mb-2">
+      02.
+      <v-contrainer>
+        <v-row no-gutters>
+          <v-col>
+            <v-card class="pa-2" outlined tile>
+              <span class="text-body-1">Bb</span>
+              <span class="text-body-1">7</span>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card class="pa-2" outlined tile>
+              <span class="text-body-1">Bb</span>
+              <span class="text-body-1">7</span>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card class="pa-2" outlined tile>
+              <span class="text-body-1">F</span>
+              <span class="text-body-1">7</span>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card class="pa-2" outlined tile>
+              <span class="text-body-1">A</span>
+              <span class="text-body-1">φ</span>
+              <span class="text-center">
+                <span class="text-body-1">D</span>
+                <span class="text-body-1">7</span>
+              </span>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-contrainer>
+    </div>
+    <div class="mb-2">
+      03.
+      <v-contrainer>
+        <v-row no-gutters>
+          <v-col>
+            <v-card class="pa-2" outlined tile>
+              <span class="text-body-1">G</span>
+              <span class="text-body-1">m7</span>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card class="pa-2" outlined tile>
+              <span class="text-body-1">C</span>
+              <span class="text-body-1">7</span>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card class="pa-2" outlined tile>
+              <span class="text-body-1">F</span>
+              <span class="text-body-1">7</span>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card class="pa-2" outlined tile>
+              <span class="text-body-1">C</span>
+              <span class="text-body-1">7</span>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-contrainer>
+    </div>
   </v-container>
 </template>
 
 <script>
+import chord from "@/assets/chord";
+
 export default {
-  data: () => ({}),
+  data: () => ({
+    rootAll: chord.rootAll,
+    chordAll: chord.chordAll,
+    title: "Now's the time",
+    key: "F",
+  }),
 };
 </script>
