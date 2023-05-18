3ddb094773ed3313d84bce7f50b9866665ae4e67
kdingn <koukou3335@gmail.com>
2023-05-18 21:02:26 +0900

    grid 1段 作成

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,21 +1,19 @@
 <template>
   <v-container>
-    <v-text-field v-model="message" label="v-text-field"></v-text-field>
-    <v-card color="primary">
-      <v-card-subtitle>
-        <span class="white--text">v-card</span>
-      </v-card-subtitle>
-      <v-card-text>
-        <span class="white--text">{{ message }}</span>
-      </v-card-text>
-    </v-card>
+    <v-contrainer>
+      <v-spacer></v-spacer>
+      aaa
+      <v-row no-gutters>
+        <v-col v-for="n in 3" :key="n">
+          <v-card class="pa-2" outlined tile> なかみ </v-card>
+        </v-col>
+      </v-row>
+    </v-contrainer>
   </v-container>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    message: "メッセージです",
-  }),
+  data: () => ({}),
 };
 </script>
