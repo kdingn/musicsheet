0add1e1209f96b5bcecf92911367d6e74a5a4525
kdingn <koukou3335@gmail.com>
2023-05-18 22:48:16 +0900

    header の追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,10 @@
   <v-toolbar color="primary">
     <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon>
     <v-toolbar-title class="white--text">{{ title }}</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon href="https://github.com/kdingn/publicpage">
+      <v-icon color="white" size="x-large">mdi-github</v-icon>
+    </v-btn>
   </v-toolbar>
 </template>
 
